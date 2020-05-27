63c13410a1f599d09660247acbf33c73f9ed9c65
oba18 <m.oba.0426@gmail.com>
2020-05-27 17:14:40 +0900

    update

--- dataset.py
+++ dataset.py
@@ -15,8 +15,51 @@ class Dataset(BaseDataset):
                     }
                 ]
 
+    # 任意のtextureの値のアノテーションデータを全て取り出す
+    def get_texture(self, texture_val):
+        return pl.query(pl.Annotation).filter(pl.Annotation.texture <= texture_val).all()
+
+    def get_scan(self, annotation):
+        return (pl.query(pl.Scan).filter(pl.Scan.id == annotation.scan_id)).all()
+
+    # これの返り値が参照するディレクトリの名前になる
+    def get_patient_id(self, scan):
+        return scan.patient_id
+
+    # アノテーションされたスキャンデータからアノテーションされているスライス面のスライス番号を返す
+    def get_slices(self, scan):
+        slices = [np.array([a.centroid for a in gourp]).mean(0) for gourp in scan.cluster_annotations()]
+        slice_list = []
+        for a in range(len(slices)):
+            a_slice = int(slices[a][2])
+            slice_list.append(a_slice)
+        return slice_list
+
+    def get_path(self, dir_name):
+        init_path = '/Volumes/masashi/workspace/0_KML/0_medical/0_data/LIDC-IDRI/'
+        if len(glob.glob(init_path + dir_name + '/*')) >= 2:
+            return 'a'
+        else:
+            return 'b'
+
     def __getitem__(self, index):
         return self.data[index]
     
     def __len__(self):
         return len(self.data)
+
+d = Dataset()
+d.ann_index = 0
+scan_index = 0
+for i, ann_index in enumerate(range(1012)):
+    ann = d.get_texture(1)
+    print ('ann', ann[ann_index])
+    scan = d.get_scan(ann[ann_index])
+    print ('scan', scan)
+    patient_id = d.get_patient_id(scan[scan_index])
+    print ('patient_id', patient_id)
+    slices = d.get_slices(scan[scan_index])
+    print ('slices', slices)
+    path = d.get_path(patient_id)
+    print (i+1, ':', path)
+    print ('----------------------------------')
--- test_get_lidc.py
+++ test_get_lidc.py
@@ -10,13 +10,19 @@ def get_scan_data():
         scans = pl.query(pl.Scan)[i]
         return scans
 
+# Annotationデータから欲しいデータのScanデータを取り出す
+def get_scan_from_ann(ann):
+    scan_ann = pl.query(pl.Scan).filter(pl.Scan.id == ann.scan_id)
+    return scan_ann
+
 # textureが1のもの
 def get_ann_data():
     for i in range(len(pl.query(pl.Annotation).filter(pl.Annotation.texture == 1).all())):
+        # textureが1のもののスキャンデータを取る
         anns = pl.query(pl.Annotation).filter(pl.Annotation.texture == 1)[i]
-        # return anns
-        print(anns)
-        print(anns.scan)
+        print (anns.cluster_annotations())
+        # print (get_scan_from_ann(anns).all())
+
 
 # scanの中からアノテーションされたものを取り出す
 def get_ann_from_scan():
@@ -26,4 +32,30 @@ def get_ann_from_scan():
         print (nodules)
         return nodules
 
-get_ann_data()
+def get_path(dir_name):
+    init_path = '/Volumes/masashi/workspace/0_KML/0_medical/0_data/LIDC-IDRI/'
+    if len(glob.glob(init_path + dir_name + '/*')) >= 2:
+        return 'a'
+    else:
+        return 'b'
+
+def get_visual(i):
+    ann = pl.query(pl.Annotation).filter(pl.Annotation.texture == 1)[i]
+    scan = pl.query(pl.Scan).filter(pl.Scan.id == ann.scan_id)[i]
+    scan.visualize(annotation_groups = scan.cluster_annotations())
+
+
+ann_index = 0
+scan_index = 0
+for i, ann_index in enumerate(range(1012)):
+    ann = get_texture(1)
+    print ('ann', ann[ann_index])
+    scan = get_scan(ann[ann_index])
+    print ('scan', scan)
+    patient_id = get_patient_id(scan[scan_index])
+    print ('patient_id', patient_id)
+    slices = get_slices(scan[scan_index])
+    print ('slices', slices)
+    path = get_path(patient_id)
+    print (i+1, ':', path)
+    print ('----------------------------------')

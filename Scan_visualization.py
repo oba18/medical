ee255e1fe87b23ca8d02d15d523f72eb9b4b904a
oba18 <m.oba.0426@gmail.com>
2020-05-28 18:51:23 +0900

    update

--- Scan_visualization.py
+++ Scan_visualization.py
@@ -1,16 +1,20 @@
 import pylidc as pl
 
-# ann = pl.query(pl.Annotation).filter(pl.Annotation.texture==1)
+ann = pl.query(pl.Annotation).filter(pl.Annotation.texture==1)[1]
+scan = pl.query(pl.Scan).filter(pl.Scan.patient_id == 'LIDC-IDRI-0003')
+print (ann)
 # scan_ann = pl.query(pl.Scan).filter(pl.Scan.id == ann.first().scan_id)
 # scan = pl.query(pl.Scan)[12]
 
-ann = pl.query(pl.Annotation)[0]
+# ann = pl.query(pl.Annotation)[0]
 # scan_ann = pl.query(pl.Scan).filter(pl.Scan.id == ann.scan_id)
-scan = pl.query(pl.Scan)[0]
+scan = pl.query(pl.Scan).filter(pl.Scan.id == ann.scan_id)[0]
+print (scan)
 
 # ann.visualize_in_scan()
-# scan.visualize(annotation_groups=scan.cluster_annotations())
-ann_scan = ann.scan.to_volume().shape
-print (ann_scan)
-ann.scan.visualize(annotation_groups=scan.cluster_annotations())
+# ann.visualize_in_scan()
+scan.visualize(annotation_groups=scan.cluster_annotations())
+# ann_scan = ann.scan.to_volume().shape
+# print (ann_scan)
+# ann.scan.visualize(annotation_groups=scan.cluster_annotations())
 
